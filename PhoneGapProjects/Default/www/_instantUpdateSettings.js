be43e8c09251059260387265167c3f1b2ad53def
paulammoun <paul@pcs-lb.com>
2019-05-26 17:45:43 +0300

    initial commit

---
+++ PhoneGapProjects/Default/www/_instantUpdateSettings.js
@@ -0,0 +1,12 @@
+function _instantUpdateSettings() {
+return {
+	"baseLineGUID": "7fc4109cbeef41b9b9720ef170af2f5a",
+	"baseURL": "http://phonegapinstantupdate.s3.amazonaws.com/",
+	"displayMessageAfterUpdate": false,
+	"message": "",
+	"systemMessages": "Verbose",
+	"autoUpdate": true,
+	"displayFirstTimeLoadMessage": true,
+	"firstTimeRunMessage": ""
+};
+}
